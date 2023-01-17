6aa22a6872131f7aa40cf489cfd678d3ba789854
ximenanavarro <ximenanavarro@Ximenas-MacBook-Pro.local>
2023-01-17 09:43:59 -0700

    commit tree f8b1afe91eeb4c8da46812738b23eeecbc4f8106

--- db/migrate/20210506153127_create_rides.rb
+++ db/migrate/20210506153127_create_rides.rb
@@ -1,6 +1,10 @@
 class CreateRides < ActiveRecord::Migration[6.1]
   def change
     create_table :rides do |t|
+      # add_column :rides, :taxi_id, :integer
+      # add_column :rides, :passenger_id, :integer
+      t.belongs_to :passenger#, foreign_key :passenger_id
+      t.belongs_to :taxi#, foreign_key :taxi_id
 
       t.timestamps
     end
